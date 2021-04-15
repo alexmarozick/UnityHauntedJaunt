96a161485368026046c72408328efa3ba7d4b44b
arcane <kevindraw@comcast.net>
2021-04-15 15:10:26 -0700

    added interpolation

---
+++ HauntedJaunt/Assets/Scripts/LightChange.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightChange : MonoBehaviour
+{
+    public GameObject traveling;
+    public GameObject endpoint;
+    Light m_Light;
+    Vector3 pos;
+    Vector3 pos2;
+
+    public Vector4 startcolor;
+    public Vector4 endcolor;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Transform basiclevel = endpoint.GetComponent<Transform>();
+        pos = basiclevel.position;
+        m_Light = GetComponent<Light>();
+
+        startcolor = startcolor / 255;
+        endcolor = endcolor / 255;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos2 = traveling.GetComponent<Transform>().position;
+        Vector3 diff = pos2 - pos;
+        float level = diff.magnitude;
+        float color1 = 1.0f / 1600f * (level * level);
+        m_Light.color = Vector4.Lerp(endcolor, startcolor, color1);
+    }
+}
